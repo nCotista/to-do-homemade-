9f144cf9ea0c451542032b437b7bfd88736ee029
slxyzpt <126223816+slxyz2508@users.noreply.github.com>
2023-08-19 13:40:40 +0700

    Update script1.js

--- script1.js
+++ script1.js
@@ -6,7 +6,59 @@ function createCanvas() {
     let dCanvas = document.createElement('div')
     dCanvas.textContent =  `work ${i}` 
     dCanvas.setAttribute('id',i)
-    dBuilt.appendChild(dCanvas)
+    dCanvas.setAttribute("class",'folder')
+    dBuilt.appendChild(dCanvas) 
+    return i;
+}
+// 
+// function addbt() {
+//     let dBuilt = document.getElementById('1')
+//     let i = dBuilt.childElementCount+1
+//     let dCanvas = document.createElement("div")
+//     dCanvas.textContent =  `+` 
+//     dCanvas.setAttribute('id','1header')
+//     dBuilt.appendChild(dCanvas)
+// }
+dragElement(document.getElementById('mydiv'));
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
 
 function newtask(){
@@ -22,13 +74,9 @@ function on() {
 const clickButton = () => {
     const test = document.getElementById('createCanvas')
     test.addEventListener('click', createCanvas)
-    test.addEventListener('click', addbt)
-}
-=======
 }
 
 
->>>>>>> Stashed changes
 
 
 // function createFolder(){
